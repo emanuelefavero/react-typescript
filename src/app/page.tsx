5699b4c70d57ee1f16041f435e35cc1b557e1b0d
Emanuele Favero <emafaux89@gmail.com>
2025-03-19 19:13:43 +0100

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <h1>React TypeScript</h1>
+      <h1 className='font-bold text-3xl'>React TypeScript</h1>
 
       <Link href='/void'>Void</Link>
     </>
