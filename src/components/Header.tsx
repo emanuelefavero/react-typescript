4ba8a84691aff494597bf76d4342a220894f50cf
Emanuele Favero <emafaux89@gmail.com>
2025-04-03 09:01:33 +0200

    feat: add back button in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,20 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 export default function Component() {
   const pathname = usePathname()
-  return <>{pathname === '/' ? 'Home' : <Link href='/'>Home</Link>}</>
+  const router = useRouter()
+
+  if (pathname === '/') return 'Home'
+
+  return (
+    <div className='flex gap-2 mb-2'>
+      <Link href='/'>Home</Link>
+      <button onClick={() => router.back()} className='link'>
+        Go Back
+      </button>
+    </div>
+  )
 }
