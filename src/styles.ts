848ca03984f55f50217db4ff90bc5d152687da89
Emanuele Favero <emafaux89@gmail.com>
2025-04-04 18:34:08 +0200

    feat: update input styles

--- src/styles.ts
+++ src/styles.ts
@@ -1,3 +1,3 @@
 // TIP: If you add /*tw*/ before the string, the Tailwind CSS IntelliSense extension will work even here in TypeScript files
 export const INPUT_STYLES =
-  /*tw*/ 'border border-gray-300 rounded-md p-2 mr-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
+  /*tw*/ 'border border-gray-300 rounded-md p-2 mr-2 focus:outline-none focus:border-blue-500'
