ac91805e493fff85c5db7f894b69e09ff393e06f
Emanuele Favero <emafaux89@gmail.com>
2025-04-04 17:27:33 +0200

    refactor: rename Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   ref?: Ref<HTMLButtonElement>
 }
 
-export default function Button({ className, ref, ...props }: ButtonProps) {
+export default function Component({ className, ref, ...props }: ButtonProps) {
   return (
     <button
       className={`bg-blue-500 text-white rounded-md p-2 cursor-pointer transition-transform duration-200 hover:bg-blue-600 active:scale-95 ${
