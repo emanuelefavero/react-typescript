4ebb40c10d524041bd8b88a11b2648087907b856
Emanuele Favero <emafaux89@gmail.com>
2025-08-17 17:50:52 +0200

    refactor: move fetch client example 1 to example 1 route

--- src/app/fetch-client/page.tsx
+++ src/app/fetch-client/page.tsx
@@ -1,23 +1,10 @@
-import { baseUrl } from '@/config/baseUrl'
-import { Suspense } from 'react'
-import Component from './Component'
-
-async function getPosts() {
-  const res = await fetch(`${baseUrl}/api/posts`)
-  return await res.json()
-}
-
-// * You can use React's use hook to stream data from the server to client. Start by fetching data in your Server component...(@see ./Component)
+import Link from 'next/link'
 
 export default function Page() {
-  const posts = getPosts()
-
   return (
     <>
       <h1>Fetch in Client Components</h1>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Component posts={posts} />
-      </Suspense>
+      <Link href='/fetch-client/example-1'>Example 1</Link>
     </>
   )
 }
