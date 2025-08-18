8ecbed0dffce64074c308514599fae9d3915618a
Emanuele Favero <emafaux89@gmail.com>
2025-08-18 12:54:14 +0200

    refactor: /fetch-client actions

--- src/app/fetch-client/actions.ts
+++ src/app/fetch-client/actions.ts
@@ -4,25 +4,32 @@ import type { Post } from '@/types/posts'
 import { readPosts, writePosts } from '@/utils/posts'
 import { revalidatePath } from 'next/cache'
 
-export async function addPost(
-  formData: FormData,
-): Promise<{ success?: boolean; error?: string }> {
+type AddPostResponse = Promise<{ success?: boolean; error?: string }>
+
+export async function addPost(formData: FormData): AddPostResponse {
+  // Get title and content from form data
   const title = formData.get('title') as string
   const content = formData.get('content') as string
 
-  if (!title || !content) {
-    return { error: 'Title and content are required' }
-  }
+  if (!title || !content) return { error: 'Title and content are required' }
 
+  // Create a new post object
   const newPost: Post = {
     id: crypto.randomUUID(),
     title,
     content,
   }
+
   // Read current posts from file
   const posts = await readPosts()
+
+  // Add the new post to the array
   posts.push(newPost)
+
+  // Write the updated posts array back to the file
   await writePosts(posts)
+
   revalidatePath('/fetch-client') // Update the post list cache
+
   return { success: true }
 }
