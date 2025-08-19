f30e981e6772e19d884c9b81fadd00eb931807d7
Emanuele Favero <emafaux89@gmail.com>
2025-08-19 18:23:29 +0200

    feat: add useOptimistic hook to /use-optimistic

--- src/app/use-optimistic/Likes.tsx
+++ src/app/use-optimistic/Likes.tsx
@@ -2,26 +2,44 @@
 
 import Button from '@/components/shared/Button'
 import { baseUrl } from '@/config/baseUrl'
-import { useEffect, useState } from 'react'
+import { startTransition, useEffect, useOptimistic, useState } from 'react'
 
 export default function Component() {
   const [likes, setLikes] = useState<number>(0)
 
+  // optimistic hook: we define how the state should update optimistically
+  const [optimisticLikes, addOptimisticLike] = useOptimistic(
+    likes,
+    (state, newLike: number) => state + newLike,
+  )
+
+  // Show initial likes
   useEffect(() => {
     fetch(`${baseUrl}/api/likes`)
       .then((res) => res.json())
       .then((data) => setLikes(data.likes))
   }, [])
 
+  // * Update like optimistically
   const handleLike = async () => {
-    const res = await fetch(`${baseUrl}/api/likes`, { method: 'POST' })
-    const data = await res.json()
-    setLikes(data.likes)
+    startTransition(() => {
+      addOptimisticLike(1) // Update UI instantly
+    })
+
+    // Send request to server
+    try {
+      const res = await fetch(`${baseUrl}/api/likes`, { method: 'POST' })
+      const data = await res.json()
+      setLikes(data.likes) // Reconcile with server state
+    } catch (error) {
+      console.error(error)
+      setLikes(likes) // If the request fails, reset to the previous likes count
+    }
   }
 
   return (
     <>
-      <p className='mb-2'>Likes: {likes}</p>
+      <p className='mb-2'>Likes: {optimisticLikes}</p>
       <Button onClick={handleLike}>Like</Button>
     </>
   )
